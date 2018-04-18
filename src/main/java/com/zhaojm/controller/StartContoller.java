d2199351f559ab0a9e971236e0155b1255aa2fe4
zhaojiamin <zhaojiamin@szhtxx.com>
2018-04-18 17:02:21 +0800

    html页面--后台管理基本页面

--- src/main/java/com/zhaojm/controller/StartContoller.java
+++ src/main/java/com/zhaojm/controller/StartContoller.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class StartContoller {
     
-    @RequestMapping("/index")
+    @RequestMapping("/login")
     public String login(){
         return "login";
+    }
+    
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
     }
 
 }
